89e790ac4d572f07c8797875f043e27a046ec081
Erik Campobadal <soc@erik.cat>
2020-06-03 08:24:33 +0200

    Updated

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,222 +1,150 @@
 import { colorPalette, Hooks as BaseHooks } from '@chartisan/chartisan'
 import {
-    ChartType,
-    Scriptable,
-    ChartColor,
-    ChartDataSets,
-    ChartTitleOptions,
-    ChartLegendOptions,
-    ChartTooltipOptions,
-    ChartAnimationOptions,
-    ChartLayoutPaddingObject
+  ChartType,
+  Scriptable,
+  ChartColor,
+  ChartDataSets,
+  ChartTitleOptions,
+  ChartLegendOptions,
+  ChartTooltipOptions,
+  ChartAnimationOptions,
+  ChartLayoutPaddingObject,
 } from 'chart.js'
 import { CC } from './index'
 
 /**
  * Used as the interface for the types hook.
- *
- * @export
- * @interface TypesHook
- * @extends {ChartDataSets}
  */
 export interface DatasetHook extends ChartDataSets {
-    type: ChartType | string
+  type: ChartType | string
 }
 
 export class Hooks extends BaseHooks<CC> {
-    /**
-     * Used to set the color color of a dataset.
-     *
-     * @param {*} [colors=colorPalette]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    colors(
-        colors: (
-            | ChartColor
-            | ChartColor[]
-            | Scriptable<ChartColor>
-        )[] = colorPalette
-    ): this {
-        return this.custom(({ data }) => {
-            if (data.data?.datasets)
-                data.data.datasets = data.data.datasets.map(
-                    (dataset, index) => ({
-                        ...dataset,
-                        borderColor: colors[index % colors.length],
-                        backgroundColor: colors[index % colors.length]
-                    })
-                )
-            return data
-        })
-    }
+  /**
+   * Used to set the color color of a dataset.
+   */
+  colors(colors: (ChartColor | ChartColor[] | Scriptable<ChartColor>)[] = colorPalette): this {
+    return this.custom(({ data }) => {
+      if (data.data?.datasets)
+        data.data.datasets = data.data.datasets.map((dataset, index) => ({
+          ...dataset,
+          borderColor: colors[index % colors.length],
+          backgroundColor: colors[index % colors.length],
+        }))
+      return data
+    })
+  }
 
-    /**
-     * Used to make the chart responsive.
-     *
-     * @param {boolean} [maintainAspectRatio=true]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    responsive(value = true): this {
-        return this.options({ options: { maintainAspectRatio: !value } })
-    }
+  /**
+   * Used to make the chart responsive.
+   */
+  responsive(value = true): this {
+    return this.options({ options: { maintainAspectRatio: !value } })
+  }
 
-    /**
-     * Set the chart legend options. If a boolean is
-     * supplied, it will be used as the display value.
-     *
-     * @param {(boolean | ChartLegendOptions)} [legend={}]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    legend(legend: boolean | ChartLegendOptions = {}): this {
-        if (typeof legend === 'boolean') legend = { display: legend }
-        return this.options({ options: { legend } })
-    }
+  /**
+   * Set the chart legend options. If a boolean is
+   * supplied, it will be used as the display value.
+   */
+  legend(legend: boolean | ChartLegendOptions = {}): this {
+    if (typeof legend === 'boolean') legend = { display: legend }
+    return this.options({ options: { legend } })
+  }
 
-    /**
-     * Determines if the chart will show the axes.
-     *
-     * @param {boolean} [display=true]
-     * @param {boolean} [strict=false]
-     * @returns {Hook}
-     * @memberof Hooks
-     */
-    displayAxes(display = true, strict = false): this {
-        return this.options(
-            strict
-                ? { options: { scale: { display } } }
-                : {
-                      options: {
-                          scales: { xAxes: [{ display }], yAxes: [{ display }] }
-                      }
-                  }
-        )
-    }
-
-    /**
-     * Creates a minimalist chart.
-     *
-     * @param {boolean} [value=true]
-     * @returns {Hook}
-     * @memberof Hooks
-     */
-    minimalist(value = true): this {
-        this.legend({ display: !value })
-        return this.displayAxes(!value)
-    }
-
-    /**
-     * Determines the padding of the chart.
-     *
-     * @param {(number | ChartLayoutPaddingObject)} [padding=5]
-     * @returns
-     * @memberof Hooks
-     */
-    padding(padding: number | ChartLayoutPaddingObject = 5): this {
-        return this.options({ options: { layout: { padding } } })
-    }
-
-    /**
-     * Set the dataset options. If a single string is provided, all the datasets
-     * will will be specified to the desired type. If an array is provided, if the
-     * inner element is a string, it will be used as a type, otherwise, the options
-     * will be applied, in both cases, to the nth position. Keep in mind that if the
-     * array length is less than the datasets, it will start from the start again.
-     *
-     * @param {(ChartType | (ChartType | DatasetHook)[])} types
-     * @param {string} [general='bar']
-     * @returns {this}
-     * @memberof Hooks
-     */
-    datasets(
-        types: ChartType | (ChartType | DatasetHook)[],
-        general = 'bar'
-    ): this {
-        return this.custom(({ data }) => {
-            data.type = typeof types === 'string' ? types : general
-            if (Array.isArray(types) && data.data?.datasets) {
-                const t = types.map(e =>
-                    typeof e === 'string' ? { type: e } : e
-                )
-                data.data.datasets = data.data.datasets.map(
-                    (dataset, index) => ({
-                        ...dataset,
-                        ...t[index % t.length]
-                    })
-                )
-            }
-            return data
-        })
-    }
-
-    /**
-     * Sets a title to the chart.
-     *
-     * @param {(string | ChartTitleOptions)} [title={}]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    title(title: string | ChartTitleOptions = {}): this {
-        return this.options({
+  /**
+   * Determines if the chart will show the axes.
+   */
+  displayAxes(display = true, strict = false): this {
+    return this.options(
+      strict
+        ? { options: { scale: { display } } }
+        : {
             options: {
-                title:
-                    typeof title === 'string'
-                        ? { text: title, display: true }
-                        : { display: true, ...title }
-            }
-        })
-    }
+              scales: { xAxes: [{ display }], yAxes: [{ display }] },
+            },
+          }
+    )
+  }
 
-    /**
-     * Set the chart to begin at zero.
-     *
-     * @param {boolean} [beginAtZero=true]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    beginAtZero(beginAtZero = true): this {
-        return this.options({
-            options: { scales: { yAxes: [{ ticks: { beginAtZero } }] } }
-        })
-    }
+  /**
+   * Creates a minimalist chart.
+   */
+  minimalist(value = true): this {
+    this.legend({ display: !value })
+    return this.displayAxes(!value)
+  }
 
-    /**
-     * Sets the tooltip options for the chart.
-     *
-     * @param {(boolean | ChartTooltipOptions)} tooltips
-     * @returns {this}
-     * @memberof Hooks
-     */
-    tooltip(tooltips: boolean | ChartTooltipOptions): this {
-        if (typeof tooltips === 'boolean') tooltips = { enabled: tooltips }
-        return this.options({ options: { tooltips } })
-    }
+  /**
+   * Determines the padding of the chart.
+   */
+  padding(padding: number | ChartLayoutPaddingObject = 5): this {
+    return this.options({ options: { layout: { padding } } })
+  }
 
-    /**
-     * Sometimes you need a very complex legend. In these cases,
-     * it makes sense to generate an HTML legend. Charts provide
-     * a generateLegend() method on their prototype that returns an
-     * HTML string for the legend. To configure how this legend
-     * is generated, you can change the legendCallback config property.
-     *
-     * @param {(chart: Chart) => string} legendCallback
-     * @returns {this}
-     * @memberof Hooks
-     */
-    legendCallback(legendCallback: (chart: Chart) => string): this {
-        return this.options({ options: { legendCallback } })
-    }
+  /**
+   * Set the dataset options. If a single string is provided, all the datasets
+   * will will be specified to the desired type. If an array is provided, if the
+   * inner element is a string, it will be used as a type, otherwise, the options
+   * will be applied, in both cases, to the nth position. Keep in mind that if the
+   * array length is less than the datasets, it will start from the start again.
+   */
+  datasets(types: ChartType | (ChartType | DatasetHook)[], general = 'bar'): this {
+    return this.custom(({ data }) => {
+      data.type = typeof types === 'string' ? types : general
+      if (Array.isArray(types) && data.data?.datasets) {
+        const t = types.map((e) => (typeof e === 'string' ? { type: e } : e))
+        data.data.datasets = data.data.datasets.map((dataset, index) => ({
+          ...dataset,
+          ...t[index % t.length],
+        }))
+      }
+      return data
+    })
+  }
 
-    /**
-     * Configures the animations of chart.js.
-     *
-     * @param {ChartAnimationOptions} animation
-     * @returns {this}
-     * @memberof Hooks
-     */
-    animation(animation: ChartAnimationOptions): this {
-        return this.options({ options: { animation } })
-    }
+  /**
+   * Sets a title to the chart.
+   */
+  title(title: string | ChartTitleOptions = {}): this {
+    return this.options({
+      options: {
+        title: typeof title === 'string' ? { text: title, display: true } : { display: true, ...title },
+      },
+    })
+  }
+
+  /**
+   * Set the chart to begin at zero.
+   */
+  beginAtZero(beginAtZero = true): this {
+    return this.options({
+      options: { scales: { yAxes: [{ ticks: { beginAtZero } }] } },
+    })
+  }
+
+  /**
+   * Sets the tooltip options for the chart.
+   */
+  tooltip(tooltips: boolean | ChartTooltipOptions): this {
+    if (typeof tooltips === 'boolean') tooltips = { enabled: tooltips }
+    return this.options({ options: { tooltips } })
+  }
+
+  /**
+   * Sometimes you need a very complex legend. In these cases,
+   * it makes sense to generate an HTML legend. Charts provide
+   * a generateLegend() method on their prototype that returns an
+   * HTML string for the legend. To configure how this legend
+   * is generated, you can change the legendCallback config property.
+   */
+  legendCallback(legendCallback: (chart: Chart) => string): this {
+    return this.options({ options: { legendCallback } })
+  }
+
+  /**
+   * Configures the animations of chart.js.
+   */
+  animation(animation: ChartAnimationOptions): this {
+    return this.options({ options: { animation } })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,225 +1,159 @@
 import { Hooks } from './hooks'
 import Chart, { ChartConfiguration, ChartUpdateProps } from 'chart.js'
-import {
-    isHook,
-    ServerData,
-    isChartisan,
-    Chartisan as Base
-} from '@chartisan/chartisan'
+import { isHook, ServerData, isChartisan, Chartisan as Base } from '@chartisan/chartisan'
 
 export { Hooks as ChartisanHooks }
 
 /**
  * Used as an alias.
- *
- * @type {CC}
  */
 export type CC = ChartConfiguration
 
 /**
  * Base chart class for ChartJS.
- *
- * @export
- * @class Chart
- * @extends {Base}
  */
 export class Chartisan extends Base<CC> {
-    /**
-     * The chart canvas.
-     *
-     * @type {HTMLCanvasElement}
-     * @memberof Chartisan
-     */
-    canvas?: HTMLCanvasElement
+  /**
+   * The chart canvas.
+   */
+  canvas?: HTMLCanvasElement
 
-    /**
-     * Stores the chart instance.
-     *
-     * @type {Chart}
-     * @memberof Chartisan
-     */
-    chart?: Chart
+  /**
+   * Stores the chart instance.
+   */
+  chart?: Chart
 
-    /**
-     * Helper to mutate an array manually (this is used
-     * because chartjs can only handle animations when the
-     * original array is modified).
-     *
-     * @static
-     * @template T
-     * @param {T[]} dest
-     * @param {T[]} src
-     * @param {(dest: T[], src: T[], index: number) => void} [action]
-     * @memberof Chartisan
-     */
-    static mutateArray<T>(
-        dest: T[],
-        src: T[],
-        action?: (dest: T[], src: T[], index: number) => void
-    ) {
-        let i: number
-        for (i = 0; i < src.length; i++) {
-            if (i < dest.length) {
-                if (action) {
-                    action(dest, src, i)
-                    continue
-                }
-                dest[i] = src[i]
-                continue
-            }
-            dest.push(src[i])
+  /**
+   * Helper to mutate an array manually (this is used
+   * because chartjs can only handle animations when the
+   * original array is modified).
+   */
+  static mutateArray<T>(dest: T[], src: T[], action?: (dest: T[], src: T[], index: number) => void) {
+    let i: number
+    for (i = 0; i < src.length; i++) {
+      if (i < dest.length) {
+        if (action) {
+          action(dest, src, i)
+          continue
         }
-        while (i < dest.length) dest.pop()
+        dest[i] = src[i]
+        continue
+      }
+      dest.push(src[i])
     }
+    while (i < dest.length) dest.pop()
+  }
 
-    /**
-     * Formats the data of the request to match the data that
-     * the chart needs (acording to the desired front-end).
-     *
-     * @protected
-     * @param {ServerData} response
-     * @returns {CC}
-     * @memberof Chartisan
-     */
-    protected formatData(response: ServerData): CC {
-        return {
-            type: 'bar',
-            data: {
-                labels: response.chart.labels,
-                datasets: response.datasets.map(({ name, values }) => ({
-                    label: name,
-                    data: values
-                }))
-            },
-            options: {}
-        }
+  /**
+   * Formats the data of the request to match the data that
+   * the chart needs (acording to the desired front-end).
+   */
+  protected formatData(response: ServerData): CC {
+    return {
+      type: 'bar',
+      data: {
+        labels: response.chart.labels,
+        datasets: response.datasets.map(({ name, values }) => ({
+          label: name,
+          data: values,
+        })),
+      },
+      options: {},
     }
+  }
 
-    /**
-     * Renews the canvas for another chart to be used.
-     *
-     * @protected
-     * @memberof Chartisan
-     */
-    protected renewCanvas() {
-        if (this.canvas) this.body.removeChild(this.canvas)
-        this.canvas = document.createElement('canvas')
-        this.canvas.style.width = '100%'
-        this.canvas.style.height = '100%'
-        this.body.appendChild(this.canvas)
-    }
+  /**
+   * Renews the canvas for another chart to be used.
+   */
+  protected renewCanvas() {
+    if (this.canvas) this.body.removeChild(this.canvas)
+    this.canvas = document.createElement('canvas')
+    this.canvas.style.width = '100%'
+    this.canvas.style.height = '100%'
+    this.body.appendChild(this.canvas)
+  }
 
-    /**
-     * Handles a successfull response of the chart data.
-     *
-     * @protected
-     * @param {CC} data
-     * @memberof Chartisan
-     */
-    protected onUpdate(data: CC) {
-        if (this.chart) this.chart.destroy()
-        this.renewCanvas()
-        this.chart = new Chart(this.canvas!, data)
-    }
+  /**
+   * Handles a successfull response of the chart data.
+   */
+  protected onUpdate(data: CC) {
+    if (this.chart) this.chart.destroy()
+    this.renewCanvas()
+    this.chart = new Chart(this.canvas!, data)
+  }
 
-    /**
-     * Handles a successfull response of the chart data
-     * in the background (possibly, updating the values
-     * of the chart without creating a new one).
-     *
-     * @protected
-     * @param {CC} data
-     * @param {ChartUpdateProps} [options]
-     * @memberof Chartisan
-     */
-    protected onBackgroundUpdate(data: CC, options?: ChartUpdateProps) {
-        if (this.chart) {
-            // Update the chart options.
-            this.chart.options = {
-                ...this.chart.options,
-                ...data.options
-            }
-            // Update the labels.
-            if (this.chart.data.labels && data.data?.labels) {
-                Chartisan.mutateArray(this.chart.data.labels, data.data.labels)
-            }
-            // To update the data arrays, it need to be performed
-            // to the original one. If the original one is not modified
-            // no animation will be performed. Therefore, please don't
-            // try beeing smarter here.
-            if (this.chart.data.datasets && data.data?.datasets) {
-                Chartisan.mutateArray(
-                    this.chart.data.datasets,
-                    data.data.datasets,
-                    (dest, src, i) => {
-                        Chartisan.mutateArray<
-                            (number | null | undefined) | Chart.ChartPoint
-                        >(dest[i].data!, src[i].data!)
-                    }
-                )
-            }
-            this.chart.update(options)
-        } else this.onUpdate(data)
-    }
+  /**
+   * Handles a successfull response of the chart data
+   * in the background (possibly, updating the values
+   * of the chart without creating a new one).
+   */
+  protected onBackgroundUpdate(data: CC, options?: ChartUpdateProps) {
+    if (this.chart) {
+      // Update the chart options.
+      this.chart.options = {
+        ...this.chart.options,
+        ...data.options,
+      }
+      // Update the labels.
+      if (this.chart.data.labels && data.data?.labels) {
+        Chartisan.mutateArray(this.chart.data.labels, data.data.labels)
+      }
+      // To update the data arrays, it need to be performed
+      // to the original one. If the original one is not modified
+      // no animation will be performed. Therefore, please don't
+      // try beeing smarter here.
+      if (this.chart.data.datasets && data.data?.datasets) {
+        Chartisan.mutateArray(this.chart.data.datasets, data.data.datasets, (dest, src, i) => {
+          Chartisan.mutateArray<(number | null | undefined) | Chart.ChartPoint>(dest[i].data!, src[i].data!)
+        })
+      }
+      this.chart.update(options)
+    } else this.onUpdate(data)
+  }
 
-    /**
-     * Destroys the chart instance if any.
-     *
-     * @memberof Chartisan
-     */
-    destroy() {
-        if (this.chart) this.chart.destroy()
-    }
+  /**
+   * Destroys the chart instance if any.
+   */
+  protected onDestroy() {
+    if (this.chart) this.chart.destroy()
+  }
 
-    /**
-     * This returns a base 64 encoded string of the
-     * chart in it's current state.
-     *
-     * @returns {(string | undefined)}
-     * @memberof Chartisan
-     */
-    toImage(): string | undefined {
-        return this.chart?.toBase64Image()
-    }
+  /**
+   * This returns a base 64 encoded string of the
+   * chart in it's current state.
+   */
+  toImage(): string | undefined {
+    return this.chart?.toBase64Image()
+  }
 
-    /**
-     * Returns an HTML string of a legend for that chart.
-     * The legend is generated from the legendCallback in the options.
-     *
-     * @memberof Chartisan
-     */
-    legend(): string | undefined {
-        return (this.chart?.generateLegend() as string) ?? undefined
-    }
+  /**
+   * Returns an HTML string of a legend for that chart.
+   * The legend is generated from the legendCallback in the options.
+   */
+  legend(): string | undefined {
+    return (this.chart?.generateLegend() as string) ?? undefined
+  }
 }
 
 declare global {
+  /**
+   * Extends the Window interface.
+   */
+  interface Window {
     /**
-     * Extends the Window interface.
-     *
-     * @interface Window
+     * The chartisan class to initiate it as a global variable
+     * bound to the window var.
      */
-    interface Window {
-        /**
-         * The chartisan class to initiate it as a global variable
-         * bound to the window var.
-         *
-         * @type {isChartisan}
-         * @memberof Window
-         */
-        Chartisan: isChartisan<CC>
+    Chartisan: isChartisan<CC>
 
-        /**
-         * Determines the hooks of the chart.
-         *
-         * @type {isHook<CC>}
-         * @memberof Window
-         */
-        ChartisanHooks: isHook<CC>
-    }
+    /**
+     * Determines the hooks of the chart.
+     */
+    ChartisanHooks: isHook<CC>
+  }
 }
 
 if (typeof window !== 'undefined') {
-    window.Chartisan = Chartisan
-    window.ChartisanHooks = Hooks
+  window.Chartisan = Chartisan
+  window.ChartisanHooks = Hooks
 }
